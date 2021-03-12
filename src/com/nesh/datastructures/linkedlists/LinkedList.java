95effcca9a26adc9f9bf25e0f1923056fdfbe3fc
Ignatius Ojiambo <iojiambo@payconnect.co.ke>
2021-03-12 06:46:44 +0300

    LinkedList By Mosh https://www.youtube.com/watch?v=BBpAmxU_NQo&ab_channel=ProgrammingwithMosh
    Improvements
    1. Added removeFirst, removeLast and size methods

--- src/com/nesh/datastructures/linkedlists/LinkedList.java
+++ src/com/nesh/datastructures/linkedlists/LinkedList.java
@@ -7,7 +7,7 @@ public class LinkedList {
 
   private class Node {
     private int value;
-    private Node node;
+    private Node next;
   }
 
   public void addFirst(int item) {
@@ -20,7 +20,7 @@ public class LinkedList {
       var node = new Node();
       node.value = item;
       // Link current first node to new node
-      node.node = first;
+      node.next = first;
       // Make current first node as last node
       last = first;
       // set new node as first
@@ -35,14 +35,14 @@ public class LinkedList {
       node.value = item;
 
       first = node;
-      first.node = node;
+      first.next = node;
       last = node;
     } else {
       // Create node
       var node = new Node();
       node.value = item;
       // Get last node to point to new node
-      last.node = node;
+      last.next = node;
       // Set new node as last node
       last = node;
     }
@@ -57,13 +57,43 @@ public class LinkedList {
         return index;
       }
 
-      counterNode = counterNode.node;
+      counterNode = counterNode.next;
       index++;
     }
     return -1;
   }
 
-  public boolean contains(int item){
-  return   indexOf(item) != -1;
+  public boolean contains(int item) {
+    return indexOf(item) != -1;
+  }
+
+  public void removeLast() {
+    var secondLast = first;
+    // [10 -> 20 ->30 ]
+    while (secondLast.next != last) {
+      secondLast = secondLast.next;
+    }
+    secondLast.next = null;
+    last = secondLast;
+  }
+
+  public void removeFirst() {
+    var second = first.next;
+    first.next = null;
+    first = second;
+  }
+
+  public int size() {
+    var current = first;
+    int counter = 0;
+    while (current.next != null) {
+      current = current.next;
+      counter++;
+
+      if (current.next == null) {
+        counter++;
+      }
+    }
+    return counter;
   }
 }
